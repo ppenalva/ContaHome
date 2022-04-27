8d7bf2936c6d961debc260afe254ec90d6ac7958
Pablo Penalva <ppenalva@me.com>
2022-04-27 10:23:22 +0200

    Initial Commit

---
+++ ContaHome/ContaHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContaHomeApp.swift
+//  ContaHome
+//
+//  Created by Pablo Penalva on 27/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ContaHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
